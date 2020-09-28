e805de846b308c502952d0ec561970dc2fc87904
gabengcdev <gabrielmmattos@yahoo.com.br>
2020-09-27 21:38:42 -0300

    Adding search photos btn, multiple file chooser and single file chooser

--- src/br/com/gabengcdev/logoattacher/Launcher.java
+++ src/br/com/gabengcdev/logoattacher/Launcher.java
@@ -22,5 +22,5 @@ public class Launcher extends Application{
 		
 		stage.show();
 	}
-
+	
 }
--- src/br/com/gabengcdev/logoattacher/controller/MainWindowController.java
+++ src/br/com/gabengcdev/logoattacher/controller/MainWindowController.java
@@ -1,31 +1,68 @@
 package br.com.gabengcdev.logoattacher.controller;
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 
-public class MainWindowController {
-
-	   @FXML
-	    private TextField photosAddressField;
-
+public class MainWindowController implements Initializable {
+		
 	    @FXML
 	    private TextField logoAddressField;
-
+	
+	    @FXML
+	    private TextField photosAddressField;
+	 	
+		@FXML
+		private Button btnSearchLogo;
+		
+	  	@FXML
+	    private Button btnSearchPhotos;
+	 
 	    @FXML
 	    void mergeLogoIntoPhotos() {
-	    	//ActionEvent event was inside mergeLogoIntoPhotos()
 	    	System.out.println("Clicked");
 	    }
 	    
 	    @FXML
 	    void searchLogo() {
-	    	System.out.println("Logo Clicked");
+	    	FileChooser fc = new FileChooser();
+	    	fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("PNG", "*.png"));
+	    	File f = fc.showOpenDialog(null);
+	    	if(f != null) {
+	    		logoAddressField.setText(f.getAbsolutePath());
+	    	}
 	    }
 
 	    @FXML
-	    void searchPhotos() {
-	    	System.out.println("Photos Clicked");
+	    void searchPhotos(ActionEvent event) {
+	    	FileChooser fc = new FileChooser();
+	    	fc.getExtensionFilters().addAll(
+		    		new FileChooser.ExtensionFilter("All Images", "*.*"),
+		    		new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+		    		new FileChooser.ExtensionFilter("JPEG", "*.jpeg"),
+		    		new FileChooser.ExtensionFilter("PNG", "*.png")
+		    			);
+	    	List<File> f = fc.showOpenMultipleDialog(null);
+	    	for (File file : f) {
+	    		System.out.println(file.getAbsolutePath());
+	    	}
+	    	
+	    	if(f != null) {
+					photosAddressField.setText(f.get(0).getParent());
+	    	}
 	    }
 
-	    
+		@Override
+		public void initialize(URL arg0, ResourceBundle arg1) {
+			
+		}
+
 	
 }
