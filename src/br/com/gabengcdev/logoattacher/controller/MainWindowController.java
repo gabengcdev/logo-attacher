18eee1ae58b06ca177755e241ad9f8f1c0409617
gabengcdev <gabrielmmattos@yahoo.com.br>
2020-09-28 23:35:31 -0300

    Added new folder file destiny in the main window

--- src/br/com/gabengcdev/logoattacher/controller/MainWindowController.java
+++ src/br/com/gabengcdev/logoattacher/controller/MainWindowController.java
@@ -16,13 +16,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
 public class MainWindowController implements Initializable {
-	
+
 	private static String logoSrc;
+	private static String destinyFolder;
 	private static ArrayList<String> photosList = new ArrayList<String>();
-	
+
 	@FXML
 	private TextField logoAddressField;
 
@@ -30,17 +32,20 @@ public class MainWindowController implements Initializable {
 	private TextField photosAddressField;
 
 	@FXML
+	private TextField destinyField;
+
+	@FXML
 	private Button btnSearchLogo;
 
 	@FXML
 	private Button btnSearchPhotos;
-
+	
 	@FXML
 	void mergeLogoIntoPhotos() {
 		for (String image : photosList) {
 			File sourceImageFile = new File(image);
 			File watermarkImageFile = new File(logoSrc);
-			File destImageFile = new File (image);
+			File destImageFile = new File(image);
 			addImageWatermark(sourceImageFile, watermarkImageFile, destImageFile);
 		}
 	}
@@ -58,7 +63,7 @@ public class MainWindowController implements Initializable {
 
 	@FXML
 	void searchPhotos() {
-		
+
 		FileChooser fc = new FileChooser();
 		fc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("All Images", "*.*"),
 				new FileChooser.ExtensionFilter("JPG", "*.jpg"), new FileChooser.ExtensionFilter("JPEG", "*.jpeg"),
@@ -66,44 +71,55 @@ public class MainWindowController implements Initializable {
 		List<File> f = fc.showOpenMultipleDialog(null);
 		for (File file : f) {
 			photosList.add(file.getAbsolutePath());
-			
 		}
-		
+
 		if (f != null) {
 			photosAddressField.setText(f.get(0).getParent());
 		}
 	}
+	
+	@FXML
+    void defineDestiny() {
+		DirectoryChooser directoryChooser = new DirectoryChooser();
+		File selectedDirectory = directoryChooser.showDialog(null);
+
+		if(selectedDirectory != null){
+			destinyField.setText(selectedDirectory.getAbsolutePath());
+			destinyFolder = selectedDirectory.getAbsolutePath();
+			System.out.println(destinyFolder);
+		}
+		     
+    }
+	
 
 	@Override
 	public void initialize(URL url, ResourceBundle rc) {
 
 	}
-	
-	
-	
+
 	static void addImageWatermark(File watermarkImageFile, File sourceImageFile, File destImageFile) {
-		
+
 		try {
-			
+
 			BufferedImage sourceImage = ImageIO.read(sourceImageFile);
 			BufferedImage watermarkImage = ImageIO.read(watermarkImageFile);
-			
+
 			Graphics2D g2d = (Graphics2D) sourceImage.getGraphics();
-	        AlphaComposite alphaChannel = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.3f);
-	        g2d.setComposite(alphaChannel);
-	 
-	        // calculates the coordinate where the image is painted
-	        int topLeftX = (sourceImage.getWidth() - watermarkImage.getWidth()) / 2;
-	        int topLeftY = (sourceImage.getHeight() - watermarkImage.getHeight()) / 2;
-	 
-	        // paints the image watermark
-	        g2d.drawImage(watermarkImage, topLeftX, topLeftY, null);
-	 
-	        ImageIO.write(sourceImage, "png", destImageFile);
-	        g2d.dispose();
-	 
-	        System.out.println("The image watermark is added to the image.");
-			
+			AlphaComposite alphaChannel = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.3f);
+			g2d.setComposite(alphaChannel);
+
+			// calculates the coordinate where the image is painted
+			int topLeftX = (sourceImage.getWidth() - watermarkImage.getWidth()) / 2;
+			int topLeftY = (sourceImage.getHeight() - watermarkImage.getHeight()) / 2;
+
+			// paints the image watermark
+			g2d.drawImage(watermarkImage, topLeftX, topLeftY, null);
+
+			ImageIO.write(sourceImage, "png", destImageFile);
+			g2d.dispose();
+
+			System.out.println("The image watermark is added to the image.");
+
 		} catch (IOException ex) {
 			System.err.println(ex);
 		}
